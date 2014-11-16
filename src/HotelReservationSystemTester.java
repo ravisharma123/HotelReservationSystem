d399882fd92b90b4b59fc0b89ba0561348286d54
siebenluke <siebenluke@gmail.com>
2014-11-15 17:40:36 -0800

    reverted changes

--- src/HotelReservationSystemTester.java
+++ src/HotelReservationSystemTester.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
  * 
  * 
  * Solution to group project 2 for CS151-01.
- * Copyright(C) Luke Sieben, Nathan Kong, and Ravi Sharma Im the ZenMaster
+ * Copyright(C) Luke Sieben, Nathan Kong, and Ravi Sharma
  * Version 2014-11-14
  ********************************************************/
 public class HotelReservationSystemTester {
