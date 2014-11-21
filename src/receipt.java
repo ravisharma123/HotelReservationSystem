c99d75d03d980b429e3e65c43a6f3bfe04b6fa45
Nate Kong <NateWKong@gmail.com>
2014-11-20 19:18:38 -0800

    added strategy pattern

---
+++ src/receipt.java
@@ -0,0 +1,26 @@
+/*********************************************************
+ * For printing out the receipt for hotel
+ * reservations
+ * 
+ * 
+ * Solution to group project 2 for CS151-01.
+ * Copyright(C) Luke Sieben, Nathan Kong, and Ravi Sharma
+ * Version 2014-11-14
+ ********************************************************/
+public interface receipt {
+	/**
+	 * Formats the header of the receipt
+	 * 
+	 */
+	String formatHeader(Guest guest);
+	
+	/**
+	 * formats a line of the    	
+	 */
+	String receipt(Room room);
+
+	/**
+	 * Formats footer of the receipt  	
+	 */
+	String formatFooter();
+}
---
+++ src/roomReceipt.java
@@ -0,0 +1,49 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/*********************************************************
+ * Format for the room receipt
+ * creates a receipt
+ * 
+ * Solution to group project 2 for CS151-01.
+ * Copyright(C) Luke Sieben, Nathan Kong, and Ravi Sharma
+ * Version 2014-11-14
+ ********************************************************/
+public class roomReceipt implements receipt{
+	private double total;
+
+	/**
+	 * 	Displays the user id and name
+	 */
+	public String formatHeader(Guest guest) {
+		this.total = 0;
+		String header = "Name: " + guest.getUsername() + "\nUser ID: " + guest.getUserID() + "\n";
+		
+		Calendar calendar = null;
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		return dateFormat.format( calendar.getTime() ) + "\n\n" + header;
+	}
+
+	/**
+	 * Displays reserved rooms for
+	 * the guest
+	 * 
+	 * @param guest is the guest making the reservation
+	 */
+	public String receipt(Room room) {
+		int roomPrice = room.getPrice();
+		total += roomPrice;
+		
+		return "\nRoom " + room.getRoomNumber() + ": $" + roomPrice;
+	}
+	
+
+	/**
+	 * Displays the total payment
+	 */
+	public String formatFooter() {
+
+		return "\n\n\t\tTOTAL DUE: $" + total;
+	}
+	
+}
